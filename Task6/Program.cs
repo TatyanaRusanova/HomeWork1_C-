bcea88614427d76388affd38ce8b1c86c7476389
Tatyana <kta995@mail.ru>
2022-11-24 22:56:16 +0300

    last changes

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -9,18 +9,3 @@ for (i = 2; i <= num; i += 2)
 {
         Console.Write($"{i}, ");
 }
-
-
-
-
-
-
-
-
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int i = 0;
-for (i = 2; i <= num; i += 2)
-{
-        Console.Write($"{i}, ");
-}
--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+for (i = 2; i <= num; i += 2)
+{
+        Console.Write($"{i}, ");
+}
