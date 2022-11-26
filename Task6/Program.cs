c2af70c9c305b83ee0111178a563801ce27c368b
Tatyana <kta995@mail.ru>
2022-11-26 15:03:31 +0300

    Task8

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -5,7 +5,6 @@
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-for (i = 2; i <= num; i += 2)
-{
-        Console.Write($"{i}, ");
-}
+
+if (num % 2 == 0) Console.WriteLine($"Число {num} четное");
+else Console.WriteLine($"Число {num} нечетное");
